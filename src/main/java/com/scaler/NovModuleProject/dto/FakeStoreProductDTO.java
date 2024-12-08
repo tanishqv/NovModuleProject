70b22d5b8f0e0ac49ceb4b8ef2b115bfb27296c0
tanishqv <tanishq7verma@gmail.com>
2024-12-08 23:03:12 +0530

    set up FakeStore Product DTO

---
+++ src/main/java/com/scaler/NovModuleProject/dto/FakeStoreProductDTO.java
@@ -0,0 +1,30 @@
+package com.scaler.NovModuleProject.dto;
+
+import com.scaler.NovModuleProject.models.Category;
+import com.scaler.NovModuleProject.models.Product;
+
+public class FakeStoreProductDTO {
+    // we can be picky on what values we want from the API
+    // Even if we skip the keys from API response, it is not incorrect
+    private Long id;
+    private String title;
+    private Double price;
+    private String description;
+    private String image;
+
+    // Map the FakeStore Product to Product model
+    public Product getProduct() {
+        Product product = new Product();
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+        product.setDescription(description);
+        product.setImageUrl(image);
+
+        Category category = new Category();
+        category.setTitle(category);
+        product.setCategory(category);
+        return product;
+    }
+
+}
