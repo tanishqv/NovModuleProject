d787393428c51682b797e6733f1f9bf949afe11b
tanishqv <tanishq7verma@gmail.com>
2024-12-08 22:31:12 +0530

    skeleton springboot application

---
+++ src/main/java/com/scaler/NovModuleProject/NovModuleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.NovModuleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovModuleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovModuleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/NovModuleProject/NovModuleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.NovModuleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovModuleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
