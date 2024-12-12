5e32b3025b6ad1e98828b5c4a3a4a8d744b3cbf3
tanishqv <tanishq7verma@gmail.com>
2024-12-12 07:21:53 +0530

    added getters, setters for DTO

--- src/main/java/com/scaler/NovModuleProject/dto/FakeStoreProductDTO.java
+++ src/main/java/com/scaler/NovModuleProject/dto/FakeStoreProductDTO.java
@@ -13,6 +13,54 @@ public class FakeStoreProductDTO {
     private String image;
     private String category;
 
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public void setTitle(String title) {
+	this.title = title;
+    }
+
+    public Double getPrice() {
+	return price;
+    }
+
+    public void setPrice(Double price) {
+	this.price = price;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public String getImage() {
+	return image;
+    }
+
+    public void setImage(String image) {
+	this.image = image;
+    }
+
+    public String getCategory() {
+	return category;
+    }
+
+    public void setCategory(String category) {
+	this.category = category;
+    }
+
     // Map the FakeStore Product to Product model
     public Product getProduct() {
         Product product = new Product();
@@ -26,6 +74,11 @@ public class FakeStoreProductDTO {
         cat.setTitle(category);
         product.setCategory(cat);
         return product;
+    }
+
+    @Override
+    public String toString() {
+	return "{id="+id+"\n,title="+title+"\n,price="+price+"\n,description="+description+"\n,image="+image+"\n,category="+category+"}\n";
     }
 
 }
