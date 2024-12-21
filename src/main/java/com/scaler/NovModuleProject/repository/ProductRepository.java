08b1434bd1df6a7dda76f264359e113ff690fcb4
tanishqv <tanishq7verma@gmail.com>
2024-12-21 13:37:25 +0530

    added Product,Category repositories

---
+++ src/main/java/com/scaler/NovModuleProject/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.scaler.NovModuleProject.repository;
+
+import com.scaler.NovModuleProject.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Save
+    // Get
+    // Get all
+    // delete
+    // update
+
+}
