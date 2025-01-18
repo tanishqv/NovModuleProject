43a0c6adf67e30f45b195fefcd13f6e8b3eed976
tanishqv <tanishq7verma@gmail.com>
2025-01-18 23:33:06 +0530

    added redis dependency to project

--- src/main/java/com/scaler/NovModuleProject/service/FSCategoryService.java
+++ src/main/java/com/scaler/NovModuleProject/service/FSCategoryService.java
@@ -7,6 +7,6 @@ import com.scaler.NovModuleProject.models.Product;
 import java.util.List;
 
 public interface FSCategoryService {
-    public List<String> getAllCategories() throws CategoryNotFoundException;
-    public List<Product> getProductsByCategoryTitle(String title) throws ProductNotFoundException;
+    List<String> getAllCategories() throws CategoryNotFoundException;
+    List<Product> getProductsByCategoryTitle(String title) throws ProductNotFoundException;
 }
